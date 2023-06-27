a74e26de12e182dc750de044d0532c255a01afde
X-L-R-8-8 <cc56@mahurangi.school.nz>
2023-06-27 19:34:48 +1200

    Starter verison
    A very basic minimal amount of code to just get something down and start to form the layout of the outcome. Just has the business title at the top with the starts of a navigation and the sections laid out underneath

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<?php include("banner.html"); ?>
+
+<section class="home-section" id="home">
+<h2> Home (place holder) </h2>
+
+</section>
+
+<section class="about-section" id="about">
+
+
+<h1>About me:</h1> <!--/ About me section --> 
+
+
+
+
+
+<section class="services-section" id="services">
+
+<h1>Services: </h1>  <!--/ Services section -->
+
+
+
+<section class="gallery-section" id="gallery">
+
+  <h1>Gallery:</h1> <!--/ Gallery section -->
+
+
+<section class="testimonials-section" id="testimonials">
+
+    <h1> Testimonials: </h1> 
+      
+     
