5a469d49afab7d51e58b85f67abd4295d8008fcc
X-L-R-8-8 <cc56@mahurangi.school.nz>
2023-06-27 21:45:59 +1200

    Content for sections
    I spent the whole week collecting all of the written text content needed for the outcome. This includes getting references from clients for the testimonials, writing a blurb for about me section and description of the businesses services.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 
 <?php include("banner.html"); ?>
-
 <section class="home-section" id="home">
 <h2> Home (place holder) </h2>
 
@@ -11,14 +10,31 @@
 
 <h1>About me:</h1> <!--/ About me section --> 
 
+  <p> Hi, <br> 
+  I’m Charlie, I’m 16 years old, and I’ve lived in Omaha for 5 years. After learning about the effects of CO2 pollution on the environment and the importance of sustainability as part of our role as  katiakitanga for our planet I wanted to do something positive. </p>
 
+  <p> Most people employ lawn mowing service in Omaha and none of them is Carbon neutral...until now. </p>
 
+  <p>Research from the USA shows that that gasoline-powered lawnmowers use 200 million gallons of gas per year and emit eight times more nitrogen oxides, 3,300 times more hydrocarbons, 5,000 times more carbon monoxide and more than twice the CO2 per hour of operation than electric lawnmowers. </p>
+  
+  <p> I use Meridian Energy so my batteries are charged from renewable electricity. In fact, by not buying your own lawnmower and using me you’re helping our environment and improving our air quality. </p>
+
+  <p> A new gas-powered lawnmower produces the same amount of volatile organic compounds and nitrogen oxides emissions of air pollution in one hour of operation as 11 new cars each being driven for one hour. </p>
+  </section>
 
 
 <section class="services-section" id="services">
 
 <h1>Services: </h1>  <!--/ Services section -->
 
+  <p>Regular or one-off mows All mowing with edge weedeater and blowdown State-of-the-art battery-powered Makita gear for a quieter and CO2 free cut.</p>
+
+  <p>Additional weeding and garden tidy service available.
+  We use grass clippings to mulch your plants or remove and mulch someone else’s plants if preferred.
+  Competitive pricing - Tell us what you’re currently paying and we will beat it. </p>
+
+  <p>We are Omaha locals and look after our regular customers. We keep an eye on your property. If you need us to put your rubbish out or check you locked up that’s no problem. We have even rescued cats off roofs for our clients! </p>
+  </section>
 
 
 <section class="gallery-section" id="gallery">
@@ -30,4 +46,24 @@
 
     <h1> Testimonials: </h1> 
       
-     
+        <p>
+        <b>Terry,</b> <br>
+        <em>“I changed to Envirolawns last year because Charlie does the best mowing job I’ve ever seen. He was also available to cut the grass on Thursdays before we come up for the weekend. He keeps an eye on the property and mows every two weeks.”
+        </em>
+        </p>
+
+        <p>
+        <b>Heather,</b> <br> 
+        <em>“I love that I can text Charlie and he will have my grass done ASAP. I don’t have to have a regular mowing contract.”</em>
+        </p>
+
+        <p>
+        <b>Andrew,</b> <br>
+        <em>“I couldn’t get up to Omaha during lockdown and Charlie did a great job of looking after it for me at a great price thanks to Charlie.” </em>
+        </p>
+
+        <b>Neil,</b> <br>
+        <em>“Charlie does our lawns every three weeks. He has also done some garden tidying and weeding for us. We highly recommend him.” </em>
+    </section>
+
+
