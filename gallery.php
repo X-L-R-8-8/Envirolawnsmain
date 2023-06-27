9edc67e1a3b1e9dd9f8d37b75f53db0ded2a1099
X-L-R-8-8 <cc56@mahurangi.school.nz>
2023-06-28 09:23:31 +1200

    Gallery page
    Implemented all of the photos into the outcome to makeup the gallery page

--- gallery.php
+++ gallery.php
@@ -1,8 +1,65 @@
-  <link rel="stylesheet" type="text/css" href="style.css">
+ <head>   <link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ 
+
 <body>
 <?php include("banner.php"); ?>
 
 
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/terry.png"  width="600" height="400">
+  </div>
+</div>
+
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG_1309.jpg"  width="600" height="400">
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG-1860.jpg" width="600" height="400">
+
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG-1864.jpg"  width="600" height="400">
+
+  </div>
+</div>                                       
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG-1840.jpg"  width="600" height="400">
+   
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG_1297.jpg"  width="600" height="400">
+   
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG-0589.jpg"  width="600" height="400">
+   
+  </div>
+</div><div class="responsive">
+  <div class="gallery">
+      <img src="images/IMG-1871.jpg"  width="600" height="400">
+   
+  </div>
+</div>
 
 
 </body>
