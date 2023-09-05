436d99089bd1268834d1f34fc0644cc1673cee88
X-L-R-8-8 <99365404+X-L-R-8-8@users.noreply.github.com>
2023-09-06 09:50:22 +1200

    added email verification
    added code to verify registered accounts email but the verification code isn't being emailed to the addresses

--- phplogin/register.php
+++ phplogin/register.php
@@ -46,12 +46,20 @@ if ($stmt = $conn->prepare('SELECT id, password FROM accounts WHERE username = ?
 	} else {
         
 	// Username doesn't exists, insert new account
-if ($stmt = $conn->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+if ($stmt = $conn->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)')) {
 	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+	$uniqid = uniqid();
+$stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $uniqid);
 	$stmt->execute();
-	echo 'You have successfully registered! You can now login!';
+$from    = 'noreply@yourdomain.com';
+$subject = 'Account Activation Required';
+$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+// Update the activation variable below
+$activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $uniqid;
+$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+mail($_POST['email'], $subject, $message, $headers);
+echo 'Please check your email to activate your account!';
 } else {
 	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
